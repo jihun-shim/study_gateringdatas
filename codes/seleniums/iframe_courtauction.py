7744e62b3bbbfa8e2d493b96b1d3ec1b67faca41
jihun-shim <heartrains1@gmail.com>
2024-11-06 16:23:09 +0900

    feat : add

--- codes/seleniums/iframe_courtauction.py
+++ codes/seleniums/iframe_courtauction.py
@@ -1,4 +1,4 @@
-# * 웹 크롤링 동작
+#웹 크롤링 동작
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
@@ -10,21 +10,14 @@ webdriver_manager_directory = ChromeDriverManager().install()
 def main():
     # ChromeDriver 실행
     browser = webdriver.Chrome(service=ChromeService(webdriver_manager_directory))
-
+    
     # Chrome WebDriver의 capabilities 속성 사용
     capabilities = browser.capabilities
-
-    # - 주소 입력(https://www.w3schools.com/)
-    browser.get("https://cafe.naver.com/sbabellows")
     
-    # div.board-list > div > a
-    browser.switch_to.frame("cafe_main")
-    import time
-    time.sleep(2)
-    cafe_list = browser.find_elements(by=By.CSS_SELECTOR, value = 'div.board-list > div > a')
-    for index, row in enumerate(cafe_list):
-        print(f'title : {row.text}')
-        pass
+    # - 주소 입력(https://www.w3schools.com)
+    browser.get("https://www.courtauction.go.kr/")
+    
+   
     return
 
 if __name__=='__main__':
